e0f910ba3e03b7bcd144ce26798d8da7169a8c3b
Mykhailo Sorokin <magpie_1980@ukr.net>
2025-10-24 15:10:12 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ let package = Package(
         .library(name: "MessageKit", targets: ["MessageKit"])
     ],
     dependencies: [
-        .package(url: "https://github.com/MagpieUA/InputBarAccessoryView", .upToNextMajor(from: "7.0.0")),
+        .package(url: "https://github.com/MagpieUA/InputBarAccessoryView", .branch("master")),
     ],
     targets: [
         // MARK: - MessageKit
